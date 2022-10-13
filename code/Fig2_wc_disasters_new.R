257b179d6cacd438f58f289120614833bc800fb2
Chris Free <cfree14@gmail.com>
2022-10-13 09:50:30 -0700

    disaster work

---
+++ code/Fig2_wc_disasters_new.R
@@ -0,0 +1,132 @@
+
+# Clear workspace
+rm(list = ls())
+
+# Setup
+################################################################################
+
+# Packages
+library(tidyverse)
+
+# Directories
+indir <- "data/disasters/raw"
+outdir <- "data/disasters/raw/v2"
+plotdir <- "figures"
+
+# Read data
+data_orig <- readxl::read_xlsx(file.path(outdir, "DisastersData_updated.xlsx"), sheet=2)
+
+
+# Build data
+################################################################################
+
+# Non GOA AK regions
+ak_regions_exclude <- c("Bering Sea",
+                        "Eastern Bering Sea",
+                        "Bristol Bay",
+                        "Kuskokwim River", # flows into EBS
+                        "Norton Sound",
+                        "Norton Sound (New)",
+                        "Chignik", # West of Kodiak
+                        "Chignik salmon fishery", # West of Kodiak
+                        "Chignik Management Area", # West of Kodiak
+                        "Yukon River", # flows into EBS
+                        "Nelson Lagoon", # EBS side of AI
+                        "Port Clarence")
+
+# Build data
+data <- data_orig %>%
+  # Rename
+  janitor::clean_names("snake") %>%
+  rename(disaster_id=disaster_number,
+         year=disaster_year,
+         sector=comm_rec_tribal,
+         status=status_as_of_8_4_2022,
+         mgmt_type=state_federal_tribal,
+         cause=federally_stated_anth_env_comb,
+         region=area_season_affected,) %>%
+  # Simplify
+  select(disaster_id, state, year, fishery, region, sector, mgmt_type, status, cause) %>%
+  # Reduce to states of interest
+  filter(state %in% c("California", "Oregon", "Washington", "Alaska")) %>%
+  # Format fishery sector
+  mutate(sector=ifelse(grepl("Tribal", sector), "Tribal", sector)) %>%
+  # Format fishery
+  mutate(fishery=stringr::str_to_sentence(fishery),
+         fishery=recode(fishery,
+                        "Chum"="Chum salmon",
+                        "Chinook"="Chinook salmon",
+                        "Red urchin"="Red sea urchin",
+                        "Groundfish (multispecies)"="Groundfish",
+                        "Salmon (sockeye?)"="Salmon",
+                        "Salmon (chinook?)"="Salmon",
+                        "Coastal salmon"="Salmon")) %>%
+  # Add fishery type
+  mutate(fishery_type=ifelse(grepl("salmon", tolower(fishery)), "Salmon",
+                             ifelse(grepl("crab|urchin", tolower(fishery)), "Inverts", "Other"))) %>%
+  # Format state
+  mutate(state=recode(state, "Alaska"="Alaska (GOA)"),
+         state=factor(state, levels=c("California", "Oregon", "Washington", "Alaska (GOA)"))) %>%
+  # Format status
+  mutate(status=factor(status, levels=c("Pending", "Denied", "Approved"))) %>%
+  # Format cause
+  mutate(cause=ifelse(is.na(cause), "Not officially determined", cause),
+         cause=recode(cause, "Combination"="Human/environmental"),
+         cause=factor(cause, levels=c("Environmental", "Human/environmental", "Not officially determined"))) %>%
+  # Reduce to AK GOA
+  filter(!region %in% ak_regions_exclude)
+
+
+# Inspect
+table(data$cause)
+table(data$sector)
+table(data$mgmt_type)
+table(data$status)
+
+# Inspect
+ak_regions <- data %>%
+  select(state, region) %>%
+  unique() %>%
+  filter(state=="Alaska (GOA)")
+
+
+# Plot data
+################################################################################
+
+# Setup theme
+my_theme <-  theme(axis.text=element_text(size=7),
+                   axis.title=element_blank(),
+                   legend.text=element_text(size=7),
+                   legend.title=element_text(size=8),
+                   strip.text=element_text(size=8),
+                   axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+                   # Gridlines
+                   panel.grid.major.x = element_blank(),
+                   panel.grid.minor.x = element_blank(),
+                   panel.background = element_blank(),
+                   axis.line = element_line(colour = "black"),
+                   # Legend
+                   legend.key.size = unit(0.5, "cm"),
+                   legend.background = element_rect(fill=alpha('blue', 0)))
+
+# Plot
+g <- ggplot(data, aes(x=year, y=fishery, color=cause, shape=status)) +
+  facet_grid(fishery_type~state, space="free_y", scales="free_y") +
+  # Plot heatwave
+  geom_rect(xmin=2013.5, xmax=2016.5, ymin=0, ymax=10, fill="grey90", inherit.aes = F) +
+  # Plot disasters
+  geom_point() +
+  # Labels
+  labs(x="Year", y="") +
+  scale_x_continuous(breaks=seq(1995, 2020, 5)) +
+  # Legends
+  scale_shape_manual(name="Status", values=c(3, 4, 16)) +
+  scale_color_manual(name="Cause", values=c("red", "blue", "grey30")) +
+  # Theme
+  theme_bw() + my_theme
+
+# Export plot
+ggsave(g, filename=file.path(plotdir, "Fig2_wc_disasters_new.png"),
+       width=6.5, height=3, units="in", dpi=600)
+
+
